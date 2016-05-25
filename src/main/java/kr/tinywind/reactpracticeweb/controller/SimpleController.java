9b8dc64ece6d1104fff3aa2aa2da7ebb8dd02ea1
tinywind <tinywind0@gmail.com>
2016-05-25 23:52:33 +0900

    set spring-boot

---
+++ src/main/java/kr/tinywind/reactpracticeweb/controller/SimpleController.java
@@ -0,0 +1,21 @@
+package kr.tinywind.reactpracticeweb.controller;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class SimpleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SimpleController.class, args);
+    }
+}
