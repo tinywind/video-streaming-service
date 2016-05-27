32c271ff5e8db1770ff90780c617b019e4ccb483
Jeon JaeHyeong <tinywind@linkbiz.kr>
2016-05-27 14:01:49 +0900

    set spring-test

---
+++ src/test/java/kr/tinywind/reactpracticeweb/Tests.java
@@ -0,0 +1,17 @@
+package kr.tinywind.reactpracticeweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Launcher.class)
+@WebAppConfiguration
+public class Tests {
+    @Test
+    public void contextLoads() {
+    }
+}
