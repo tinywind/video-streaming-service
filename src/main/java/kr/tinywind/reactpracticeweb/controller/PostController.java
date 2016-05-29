ed62d76324b77f2962f77b9789f90a19a185366d
tinywind <tinywind0@gmail.com>
2016-05-30 02:03:28 +0900

    add function: post's delete, modify

--- src/main/java/kr/tinywind/reactpracticeweb/controller/PostController.java
+++ src/main/java/kr/tinywind/reactpracticeweb/controller/PostController.java
@@ -1,11 +1,13 @@
 package kr.tinywind.reactpracticeweb.controller;
 
-import kr.tinywind.reactpracticeweb.service.React;
 import kr.tinywind.reactpracticeweb.model.Post;
 import kr.tinywind.reactpracticeweb.model.PostRepository;
+import kr.tinywind.reactpracticeweb.service.React;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,14 +22,34 @@ public class PostController {
     @Autowired
     private React react;
 
-    @RequestMapping(value = "/write", method = RequestMethod.GET)
-    public String form() {
+    @RequestMapping(value = "write", method = RequestMethod.GET)
+    public String form(@ModelAttribute Post post) {
         return "post/form";
     }
 
-    @RequestMapping(value = "/write", method = RequestMethod.POST)
-    public String add(Post post) {
+    @RequestMapping(value = "write", method = RequestMethod.POST)
+    public String add(Post post, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            // alert();
+            return "redirect:/post";
+        }
         post.setCreatedAt(new Date());
+        return "redirect:/post/" + postRepository.save(post).getId();
+    }
+
+    @RequestMapping(value = "{id}/modify", method = RequestMethod.GET)
+    public String modify(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("post", postRepository.findOne(id));
+        return "post/form";
+    }
+
+    @RequestMapping(value = "{id}/modify", method = RequestMethod.POST)
+    public String edit(Post post, BindingResult bindingResult, @PathVariable("id") Long id) {
+        if (bindingResult.hasErrors()) {
+            // alert();
+            return "redirect:/post";
+        }
+        post.setId(id);
         return "redirect:/post/" + postRepository.save(post).getId();
     }
 
@@ -41,5 +63,11 @@ public class PostController {
     public String page(Model model, @PathVariable("id") Long id) {
         model.addAttribute("post", postRepository.findOne(id));
         return "post/post";
+    }
+
+    @RequestMapping("{id}/delete")
+    public String delete(@PathVariable("id") Long id) {
+        postRepository.delete(id);
+        return "redirect:/post";
     }
 }
--- src/main/java/kr/tinywind/reactpracticeweb/model/Post.java
+++ src/main/java/kr/tinywind/reactpracticeweb/model/Post.java
@@ -1,6 +1,7 @@
 package kr.tinywind.reactpracticeweb.model;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
@@ -11,21 +12,22 @@ import java.util.Date;
 public class Post {
     @Id
     @GeneratedValue
-    Long id;
+    private Long id;
 
     @Size(min = 1, max = 255)
     @Column(nullable = false)
-    String title;
+    private String title;
 
     @Size(max = 255)
-    String subtitle;
+    private String subtitle;
 
     @Size(min = 1, max = 100000000)
     @Column(length = 100000000, nullable = false)
-    String content;
+    private String content;
 
     @Basic(optional = false)
     @Column(updatable = false)
     @Temporal(TemporalType.TIMESTAMP)
-    Date createdAt;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private Date createdAt;
 }
